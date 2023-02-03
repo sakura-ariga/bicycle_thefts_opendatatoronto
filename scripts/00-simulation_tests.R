a964ba75b6bb06eaf7861c09422b8900f99005dd
Sakura Ariga <sakura.ariga@mail.utoronto.ca>
2023-02-03 22:03:22 +0000

    Updated code format to test the simulated data, especially the line that imports the simulated data from the "inputs" folder.

--- scripts/00-simulation_tests.R
+++ scripts/00-simulation_tests.R
@@ -1,4 +1,7 @@
-# Test sample data 
+# Import simulated data from the inputs folder
+simulated_data <- read.csv(here::here("inputs/data/simulated_data.csv"))
+
+# Created tests to check the sample data created in 00_simulation.R file
 simulated_data$Theft_Number |> 
   class() == "numeric"
 
