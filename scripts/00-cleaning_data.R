3b83f94755cc403a6d22f50b80e6f73435652c7c
Sakura Ariga <sakura.ariga@mail.utoronto.ca>
2023-02-03 22:01:44 +0000

    Updated code format to clean the bicycle theft raw data from Open Data Toronto.

--- scripts/00-cleaning_data.R
+++ scripts/00-cleaning_data.R
@@ -2,7 +2,8 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-# Cleaned the data by ______
+# Imported the raw Bike Theft data first saved in the 00-download_data.R file
+# Cleaned this data to include just Report_Year, NeighbourhoodName, and Premises_Type, and to exclude 2022 reports
 cleaned_bike_theft_data <- 
   readr::read_csv((here::here("inputs/data/bike_theft_raw_data.csv"))) |> 
   filter(Report_Year != "2022") |> 
@@ -11,7 +12,8 @@ cleaned_bike_theft_data <-
     NeighbourhoodName,
     Premises_Type
   )
+# Saved cleaned Bike Theft data to inputs folder
 write.csv(
   x = cleaned_bike_theft_data, 
-  "/cloud/project/inputs/data/cleaned_bike_theft_data.csv"
+  "inputs/data/cleaned_bike_theft_data.csv"
 )
